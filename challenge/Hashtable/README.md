19d5387dd3505dce83946654ddd1432390793612
abuzahra98 <dawoodbasher@gmail.com>
2021-09-04 11:51:27 +0300

    edit

---
+++ challenge/Hashtable/README.md
@@ -0,0 +1,17 @@
+# Hashtables
+- Array of linked list that store key and value in tuple
+
+## Challenge
+
+- Building a hashtable using linked list for collision
+
+## Approach & Efficiency
+Big O
+
+O(n)
+
+## API
+- add: takes in both the key and value. This method should hash the key, and add the key and value pair to the table, handling - collisions as needed.
+- find: takes in the key and returns the value from the table.
+- contains: takes in the key and returns a boolean, indicating if the key exists in the table already.
+- hash_table: takes in an arbitrary key and returns an index in the collection.
---
+++ challenge/Hashtable/app/src/main/java/Hashtable/Hashtable.java
@@ -0,0 +1,60 @@
+package Hashtable;
+
+import java.util.LinkedList;
+
+public class Hashtable {
+
+    LinkedList<Object>[] bucket;
+
+
+    public Hashtable(int size){
+        this.bucket = new LinkedList[size];
+        for(int i = 0; i < bucket.length; i ++){
+            bucket[i] = new LinkedList<>();
+        }
+    }
+
+    public void add(Object key, Object value){
+        int index = hash(key);
+        bucket[index].add(value);
+    }
+
+    public LinkedList<Object> get(Object key){
+        int index = hash(key);
+        return bucket[index];
+    }
+
+    public int hash(Object value){
+        String valueAsAString = value.toString();
+        int hash = 0;
+        for(int i = 0; i < valueAsAString.length(); i ++) {
+            char character = valueAsAString.charAt(i);
+            int num = character;
+            System.out.println(num);
+            hash += num;
+            hash = hash * 27 ;
+            System.out.println(hash);
+            hash = hash % 100001;
+        }
+        hash = hash % bucket.length;
+        System.out.println("hash = " + hash);
+        return hash;
+
+    }
+    public boolean contains(Object key){
+        int index = hash(key);
+        System.out.println("index = " + index);
+        if(bucket[index].isEmpty()) return false;
+        return true;
+    }
+
+    public static class StorageObject{
+        Object key;
+        Object value;
+
+        public StorageObject(Object key, Object value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
